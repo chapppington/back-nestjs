4c77bdd70394ace14686c176af5af622af1f819f
chapppington <stas.mega.96@bk.ru>
2025-07-11 13:37:27 +0300

    order update

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -2,3 +2,8 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateProductDto } from "./create-product.dto";
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
+
+export class UpdateProductOrderDto {
+  id: string;
+  order: number;
+}
